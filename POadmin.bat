c52acebce7e01d369c16251bb29d123f7318eb90
DeadRoEd <edriok@gmail.com>
2019-08-18 18:48:38 +0300

    Revert former changes
    Change PO.bat to POadmin.bat for testing

--- POadmin.bat
+++ POadmin.bat
@@ -7,6 +7,30 @@ set ver=r6.1
 Title Performance Optimizer %ver%
 mode con: cols=62 lines=25
 
+:: BatchGotAdmin
+    :-------------------------------------
+    REM  --> Check for permissions
+    >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+    REM --> If error flag set, we do not have admin.
+    if '%errorlevel%' NEQ '0' (
+        echo Requesting administrative privileges...
+        goto UACPrompt
+    ) else ( goto gotAdmin )
+
+    :UACPrompt
+        echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+        echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+        "%temp%\getadmin.vbs"
+        exit /B
+
+    :gotAdmin
+        if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+        pushd "%CD%"
+        CD /D "%~dp0"
+    :--------------------------------------
+:: BatchGotAdminX
 
 REM --> Start Start
 
